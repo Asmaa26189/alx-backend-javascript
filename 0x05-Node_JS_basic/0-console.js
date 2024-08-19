c69ab502e8b5bcb28ba6db9462c960234da3879f
Asmaa26189 <engasmaahesham@gmail.com>
2024-08-19 13:53:20 +0300

    19-8-2024

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * displayMessage
+ * @param {String} msg input
+ * @author Asmaa Hesham <https://github.com/Asmaa26189>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
